d4378a8c1f2899679631bd081d78ccd510e08a48
joe-ashwell <67185069+joe-ashwell@users.noreply.github.com>
2020-07-21 20:44:07 +0100

    Add styling, conditional on empty and execute on enter

--- todo.js
+++ todo.js
@@ -1,10 +1,31 @@
+// Creating the variables
 const listInput = document.querySelector('input.todo-list');
-const button = document.querySelector('button.todo-list');
+const button = document.querySelector('i.todo-list');
 const ul = document.querySelector('ul.todo-list');
 
+// Executing the list elements on click
 button.addEventListener('click', () => {
-  const listItem = document.createElement('li')
-  listItem.innerHTML = listInput.value;
-  ul.appendChild(listItem);
-  listInput.value = "";
+  
+  const listItem = document.createElement('li');
+
+  // Conditional to check if the input is empty
+  if (listInput.value != "") {
+    listItem.innerHTML = listInput.value;
+    ul.appendChild(listItem);
+    listItem.innerHTML += '<i class="fa fa-trash-o delete-btn"></i>';
+    listInput.value = "";
+  } else {
+    alert("Please add some content first");
+  }
+  
+});
+
+// Having to click to create the elements was annoying me, so this now executes on enter too
+listInput.addEventListener("keyup", () => {
+
+  // Apparently, keycode 13 is the enter key
+  if (event.keyCode === 13) {
+    button.click();
+  }
+
 });
